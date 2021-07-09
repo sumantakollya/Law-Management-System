090762a4791be46c2461b06ed769c75031b7bb13
sumantakollya <sumantakollya@gmail.com>
2021-07-09 22:03:17 +0530

    views updated

--- app/views.py
+++ app/views.py
@@ -16,43 +16,38 @@ from .models import *
 from .forms import *
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
-@unauthenticated_user
+
 def registerPage(request):
 
 	form = CreateUserForm()
 	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-
-            if form.cleaned_data.get('is_lawyer'):
-                group = Group.objects.get(name='lawyer')
-                user.groups.add(group)
-                Lawyer.objects.create(
-                    user=user,
-                    name=user.username,
-                    )
-
+		user_form = CreateUserForm(request.POST)
+		if user_form.is_valid():
+            user = user_form.save()
+            username = user_form.cleaned_data.get('username')
+            if user_form.cleaned_data.get('is_lawyer'):
+                lawyer_form = LawyerForm(request.POST,request.FILES)
+                type_form = lawyer_form
+                lawyer = lawyer_form.save(commit=False)
+                lawyer.user = user
+                lawyer.save()
                 messages.success(request, 'Account was created for lawyer ' + username)
                 return redirect('login')
             else:
-                group = Group.objects.get(name='client')
-                user.groups.add(group)
-                Client.objects.create(
-                    user=user,
-                    name=user.username,
-                    )
-
+                client_form = ClientForm(request.POST,request.FILES)
+                type_form = client_form
+                client = client_form.save(commit=False)
+                client.user = user
+                client.save()
                 messages.success(request, 'Account was created for client ' + username)
                 return redirect('login')
 		
 
-	context = {'form':form}
+	context = {'user_form':user_form,'type_form':type_form}
 	return render(request, 'app/register.html', context)
 
 
-@unauthenticated_user
+
 def loginPage(request):
 
 	if request.method == 'POST':
@@ -92,7 +87,7 @@ def clientPage(request):
     print('LEGALSERVICES:', legalservices)
 
     context = {'client':client,'legalservices':legalservices, 'total_orders':total_legalservices,'completed':completed,'pending':pending}
-    return render(request, 'accounts/client.html', context)
+    return render(request, 'app/client.html', context)
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['lawyer'])
@@ -106,4 +101,35 @@ def lawyerPage(request):
     print('LEGALSERVICES:', legalservices)
 
     context = {'lawyer':lawyer,'legalservices':legalservices, 'total_orders':total_legalservices,'completed':completed,'pending':pending}
-    return render(request, 'accounts/lawyer.html', context)
+    return render(request, 'app/lawyer.html', context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['lawyer'])
+def updateLawyer(request):
+	lawyer = request.user.lawyer
+	form = LawyerForm(instance=lawyer)
+
+	if request.method == 'POST':
+		form = LawyerForm(request.POST, request.FILES,instance=lawyer)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'app/lawyer_update.html', context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['client'])
+def updateClient(request):
+	client = request.user.client
+	form = ClientForm(instance=client)
+
+	if request.method == 'POST':
+		form = ClientForm(request.POST, request.FILES,instance=client)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'app/client_update.html', context)
+
